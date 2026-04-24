6995141e650b6516c60762df6ef80fab06e27631
Hezkiel09 <kielsiahaan09@gmail.com>
2026-04-24 21:25:03 +0700

    setup express + supabase + env

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,16 @@
 const express = require('express') // ambil library express + require (import di js)
 const app = express() // inisialisasi app dengan express
-const port = 3000 // mengugnakan port 3000
+const dotenv = require('dotenv');
+dotenv.config();
+const PORT = process.env.PORT; // mengugnakan port 3000
+
+
 
 app.get('/', (req, res)=> {
   res.send('Aku PTI UB')
 }) // request GET
 
-app.listen(port, ()=>{
-  console.log(`listening port : ${port}`)
+app.listen(PORT, ()=>{
+  console.log(`listening port : ` + PORT);
 }) // respond 
+
