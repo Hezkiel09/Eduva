3201e7f9ef38e1bf75704df69ac9bf7dd7999b26
Hezkiel09 <kielsiahaan09@gmail.com>
2026-05-28 20:04:49 +0700

    feat: migration 1 table dari create_assesment foreign ke question_table

--- database/migrations/2026_05_26_131430_create_assessments_table.php
+++ database/migrations/2026_05_26_131430_create_assessments_table.php
@@ -12,7 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('assessments', function (Blueprint $table) {
-            $table->id();
+            $table->id('assessment_id');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('major')->default('RPL');
             $table->timestamps();
         });
     }
